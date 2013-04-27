b731bb4e645e614458161bf939cabc941c0f2d21
Eric Mann <eric@eamann.com>
2013-04-26 17:11:46 -0700

    Add header and footer

---
+++ root/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package {%= title %}
+ * @since 0.1.0
+ */
---
+++ root/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * The template for displaying the header.
+ *
+ * @package {%= title %}
+ * @since 0.1.0
+ */

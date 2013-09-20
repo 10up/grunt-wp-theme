efddae684883e2d3ebeafc7dba84078c90c46370
willwallace85 <kbrec85@gmail.com>
2013-09-20 14:46:02 -0700

    Added normalize.scss and other partials

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -146,7 +146,7 @@ module.exports = function( grunt ) {
 			},
 			{% } %}
 			scripts: {
-				files: ['assets/js/src/**/*.js', 'assets/js/vendor/**/*.js'],
+				files: ['assets/js/src/*.js', 'assets/js/vendor/*.js'],
 				tasks: ['jshint', 'concat', 'uglify'],
 				options: {
 					debounceDelay: 500
